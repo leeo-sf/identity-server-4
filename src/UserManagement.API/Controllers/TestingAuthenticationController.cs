539ba7b09ebb4740ac71a5440070ed22aa4184bf
Leonardo Ferreira <leonardoferreira032@gmail.com>
2025-06-09 22:56:08 -0300

    Endpoints com permissões de scopes diferentes

---
+++ src/UserManagement.API/Controllers/TestingAuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.API.Controllers;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("1.0")]
+[ControllerName("test-authentication")]
+public class TestingAuthenticationController : ControllerBase
+{
+    [HttpGet("exemplo")]
+    [Authorize(Policy = "apis.exemplo")]
+    public async Task<IActionResult> ApisExemplo()
+    {
+        return Ok("Access allowed");
+    }
+
+    [HttpGet("study")]
+    [Authorize(Policy = "apis.study")]
+    public async Task<IActionResult> ApisStudy()
+    {
+        return Ok("Access allowed");
+    }
+}
--- src/UserManagement.API/configs/Configurations.cs
+++ src/UserManagement.API/configs/Configurations.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Asp.Versioning;
+using Microsoft.OpenApi.Models;
 
 namespace UserManagement.API.configs;
 
@@ -14,7 +15,7 @@ public static class Configurations
                 opt.Authority = config.GetSection("AuthServer:authority").Value; // URL servidor de autenticação (usado para validar issuer)
                 opt.TokenValidationParameters = new TokenValidationParameters  // configuração de regras de validação do JWT
                 {
-                    ValidAudience = config.GetSection("AuthServer:audience").Value,  // valida o aud que o token deve conter
+                    ValidAudience = $"{config.GetSection("AuthServer:authority").Value}/resources",  // valida o aud que o token deve conter
                     ValidateAudience = true,  // valida se o aud no token é igual ao ValidAudience
                     ValidateIssuer = true,  // valida se o issuer no token é igual ao ValidIssuer
                     ValidIssuer = config.GetSection("AuthServer:audience").Value,
@@ -23,7 +24,14 @@ public static class Configurations
                 };
             });
 
-        services.AddAuthorization();
+        services.AddAuthorization(opt =>
+        {
+            opt.AddPolicy("apis.exemplo", policy =>
+                policy.RequireClaim("scope", "apis_exemplo"));
+
+            opt.AddPolicy("apis.study", policy =>
+                policy.RequireClaim("scope", "apis_study_project"));
+        });
     }
 
     public static void AddVersioning(this IServiceCollection services)
@@ -39,5 +47,47 @@ public static class Configurations
                 options.GroupNameFormat = "'v'VVV";
                 options.SubstituteApiVersionInUrl = true;
             });
+    }
+
+    public static void AddAuthotizationSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc(
+                "v1",
+                new OpenApiInfo { Title = "Apis.Tests", Version = "v1" }
+            );
+            c.AddSecurityDefinition(
+                "Bearer",
+                new OpenApiSecurityScheme
+                {
+                    Description = @"Enter 'Bearer' [space] and your token!",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                }
+            );
+
+            c.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                },
+                                Scheme = "oauth2",
+                                Name = "Bearer",
+                                In = ParameterLocation.Header
+                            },
+                            new List<string>()
+                        }
+                }
+            );
+        });
     }
 }
